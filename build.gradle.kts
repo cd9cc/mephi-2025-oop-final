6d8e02ce202467d8f1daf36cdf8d47918d87dd8e
Alexey Goloshchapov <april1889@gmail.com>
2025-10-30 22:44:53 +0300

    fix deprecation warning

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 
 dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.20.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
